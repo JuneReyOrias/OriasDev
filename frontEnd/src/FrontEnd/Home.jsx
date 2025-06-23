c21e0805d0f987ed1b17b2a7bd00894ab20c8e8d
ReiRoko-jutsu <117705030+JuneReyOrias@users.noreply.github.com>
2025-06-23 18:50:28 +0800

    Update Home.jsx

--- frontEnd/src/FrontEnd/Home.jsx
+++ frontEnd/src/FrontEnd/Home.jsx
@@ -60,7 +60,7 @@ function Home() {
         </a>
       
         <a
-            href="/resume.pdf" // <-- change to your actual resume file path
+            href="https://drive.google.com/file/d/1fQIOZc3yr-ajLdsr0bqXiUTOCXUlDzCe/view?usp=sharing" // <-- change to your actual resume file path
             className="text-green-500 hover:text-white transition transform hover:scale-110"
             title="Resume"
             target="_blank"
